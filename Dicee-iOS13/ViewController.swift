9b8e3c04654eaf60f08f6deca137176f8bd9ebb7
Bruce Carter <thebrucecarter@gmail.com>
2022-11-23 14:31:11 -0500

    Initial Commit

---
+++ Dicee-iOS13/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  Dicee-iOS13
+//
+//  Created by Angela Yu on 11/06/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // IBOutlet connects to dice UI elements
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+//    var leftDiceNumber = 1
+//    var rightDiceNumber = 4
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+//        Who            What    Value
+//        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
+//        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
+//        diceImageView1.alpha = 0.5
+
+    }
+
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        let diceArray = [UIImage(imageLiteralResourceName: "DiceOne"), UIImage(imageLiteralResourceName: "DiceTwo"), UIImage(imageLiteralResourceName: "DiceThree"), UIImage(imageLiteralResourceName: "DiceFour"), UIImage(imageLiteralResourceName: "DiceFive"), UIImage(imageLiteralResourceName: "DiceSix")]
+        
+//        let diceArray = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")]
+        
+//        print("Button got tapped.")
+//        diceImageView1.image = #imageLiteral(resourceName: "DiceFour")
+//        diceImageView2.image = #imageLiteral(resourceName: "DiceFour")
+        
+        diceImageView1.image = diceArray.randomElement()
+        diceImageView2.image = diceArray.randomElement()
+        
+//        diceImageView2.image = diceArray[Int.random(in: 0 ..< diceArray.count)]
+//        diceImageView2.image = diceArray[Int.random(in: 0 ... 5)]
+
+//        leftDiceNumber += 1
+//        rightDiceNumber -= 1
+    }
+    
+}
+
